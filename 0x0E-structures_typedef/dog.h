f62f52ff6324451c3784e1469486985cadefbfd4
mahi2025 <mahletmasresha08@gmail.com>
2023-08-14 05:27:12 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,7 @@ struct dog
 
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void int_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *name_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
